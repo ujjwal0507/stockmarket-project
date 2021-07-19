2c0b8d0e4e04018a030b23f47baeb021a316f483
ujjwal0507 <ujjwalsinha73@gmail.com>
2021-07-19 12:02:33 +0530

    getCompanyByExchange added to exchange-service

--- backend/exchange-service/src/main/java/com/stockmarket/exchangeservice/controller/ExchangeServiceController.java
+++ backend/exchange-service/src/main/java/com/stockmarket/exchangeservice/controller/ExchangeServiceController.java
@@ -10,16 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.stockmarket.exchangeservice.entity.Exchange;
 import com.stockmarket.exchangeservice.service.ExchangeService;
 
 @RestController
+@RequestMapping("/exchange")
 public class ExchangeServiceController {
 	
 	@Autowired
 	public ExchangeService exchangeService;
+	
+	@Autowired
+	public RestTemplate restTemplate;
 	
 	@GetMapping("/getExchange")
 	public ResponseEntity<List<Exchange>> getStockExchangeList(){
@@ -38,5 +44,11 @@ public class ExchangeServiceController {
 			return ResponseEntity.ok(exchangeOptional.get());
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Exchange with "+id+ " was not found");
+	}
+	
+	@GetMapping("/getCompanyByExchange/{exchangeId}")
+	public ResponseEntity getCompanyByExchange(@PathVariable("exchangeId") int exchangeId) {
+		String url = "http://localhost:8082/company/getCompanyByExchange/"+exchangeId;
+		return restTemplate.getForEntity(url, String.class);
 	}
 }
