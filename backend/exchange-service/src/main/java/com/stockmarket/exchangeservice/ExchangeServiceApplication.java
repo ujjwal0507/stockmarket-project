645414bf2f355064146a93c9ee1b0726412f64f1
ujjwal0507 <ujjwalsinha73@gmail.com>
2021-07-20 12:16:12 +0530

    Swagger added to all microservices

--- backend/exchange-service/src/main/java/com/stockmarket/exchangeservice/ExchangeServiceApplication.java
+++ backend/exchange-service/src/main/java/com/stockmarket/exchangeservice/ExchangeServiceApplication.java
@@ -1,11 +1,20 @@
 package com.stockmarket.exchangeservice;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ExchangeServiceApplication {
 	
 	@Bean
@@ -17,4 +26,25 @@ public class ExchangeServiceApplication {
 		SpringApplication.run(ExchangeServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.stockmarket"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Exchange Microservice API",
+				"API Documentation for Sector Microservice",
+				"1.0",
+				"Copyright- Ujjwal Sinha",
+				new springfox.documentation.service.Contact("Ujjwal Sinha", "https://github.com/ujjwal0507", "ujjwalsinha73@gmail.com"),
+				"API License",
+				"https://github.com/ujjwal0507/stockmarket-project", Collections.emptyList());
+	}
+
+	
 }
--- backend/sector-service/src/main/java/com/stockmarket/sectorservice/SectorServiceApplication.java
+++ backend/sector-service/src/main/java/com/stockmarket/sectorservice/SectorServiceApplication.java
@@ -2,12 +2,43 @@ package com.stockmarket.sectorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SectorServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SectorServiceApplication.class, args);
 	}
+	
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.stockmarket"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Sector Microservice API",
+				"API Documentation for Sector Microservice",
+				"1.0",
+				"Copyright- Ujjwal Sinha",
+				new springfox.documentation.service.Contact("Ujjwal Sinha", "https://github.com/ujjwal0507", "ujjwalsinha73@gmail.com"),
+				"API License",
+				"https://github.com/ujjwal0507/stockmarket-project", Collections.emptyList());
+	}
+
 
 }
