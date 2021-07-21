92635210b464eba45f355cfcf51f07ee348b1810
ujjwal0507 <ujjwalsinha73@gmail.com>
2021-07-22 01:57:22 +0530

    Config server error resolved

--- backend/company-service/src/main/java/com/stockmarket/companyservice/CompanyServiceApplication.java
+++ backend/company-service/src/main/java/com/stockmarket/companyservice/CompanyServiceApplication.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
@@ -15,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
